fae2d9719d858a24683b71ca0c814a519a05ec85
punitbudania <punitbudania88@gmail.com>
2020-12-28 19:14:57 +0530

    [punit]Added: used while loop to calculate montly wage by fixing the max working days

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -66,3 +66,18 @@ for (let day = 0; day<NUM_OF_WORKING_DAYS; day++)
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Total Hrs: " + empHrs + " Emp wage: " + empWage);
 
+
+// UC 5
+const MAX_HRS_IN_MONTH = 100;
+const NUM_OF_WORKING_DAYS = 10;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+{
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    totalEmpHrs += getWorkingHours(empCheck);
+}
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("UC5 - TOtal Days: " + totalWorkingDays + "Total Hrs: " + totalEmpHrs + " Emp Wage: " + empWage);
+
