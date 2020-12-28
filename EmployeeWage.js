c46bc06df4a15ffe3207fd0eeea9748b7ffc916d
punitbudania <punitbudania88@gmail.com>
2020-12-28 18:23:28 +0530

    [punit]Added: used for loop to calculate montly wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -52,5 +52,17 @@ function getWorkingHours(empCheck)
 let empHrs = 0;
 let empCheck = Math.floor(Math.random() * 10) % 3;
 empHrs = getWorkingHours(empCheck);
-empWage = empHrs * WAGE_PER_HOUR;
+let empWage = empHrs * WAGE_PER_HOUR;
 console.log("emp wage: " + empWage);
+
+// UC 4
+const NUM_OF_WORKING_DAYS = 2;
+let empHrs = 0;
+for (let day = 0; day<NUM_OF_WORKING_DAYS; day++)
+{
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs += getWorkingHours(empCheck);
+}
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Total Hrs: " + empHrs + " Emp wage: " + empWage);
+
