495a6d8c6e014f5e88537b180e9f0d7993630fec
punitbudania <punitbudania88@gmail.com>
2020-12-28 18:13:27 +0530

    [punit]Added: use function calculate daily wage based on part or full time

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -34,3 +34,23 @@ switch (empCheck)
 
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Emp wage: " + empWage);
+
+
+//UC 3
+function getWorkingHours(empCheck)
+{
+    switch (empCheck)
+    {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+let empHrs = 0;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+empHrs = getWorkingHours(empCheck);
+empWage = empHrs * WAGE_PER_HOUR;
+console.log("emp wage: " + empWage);
