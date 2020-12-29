a9710a55c77d45dac482ff5b15c78d98aee13625
punitbudania <punitbudania88@gmail.com>
2020-12-29 18:08:26 +0530

    [punit]Added: created a single object for day, hours worked and wage earned

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -161,3 +161,28 @@ console.log("Non working days: " + nonWorkingDays);
 console.log("UC 10 - Showing daily hours worked and wage earned: " + empDailyHrsAndWageArr);
 
 // UC 11
+let totalWage = empDailyHrsAndWageArr
+                .filter(dailyHrsAndWage => dailyHrsAndWage.dailyWage > 0)
+                .reduce((totalWage, dailyHrsAndWage) => totalWage += dailyHrsAndWage.dailyWage, 0);
+let totalHours = empDailyHrsAndWageArr
+                .filter(dailyHrsAndWage => dailyHrsAndWage.dailyWage > 0)
+                .reduce((totalHours, dailyHrsAndWage) => totalHours += dailyHrsAndWage.dailyHours, 0);
+console.log("UC 11A - Total hours: " + totalHours + " Total wages: " + totalWages);
+
+process.stdout.write("UC 11B")
+empDailyHrsAndWageArr.filter(dailyHrsAndWage => dailyHrsAndWage.dailyHours == 8)
+                        .forEach(dailyHrsAndWage => process.stdout.write(dailyHrsAndWage.toString()));
+                    
+let partWorkingDayStrArr = empDailyHrsAndWageArr
+                            .filter(dailyHrsAndWage => dailyHrsAndWage.dailyHours == 4)
+                            .map(dailyHrsAndWage => dailyHrsAndWage.toString());
+
+let nonWorkingDayStrArr = empDailyHrsAndWageArr
+                            .filter(dailyHrsAndWage => dailyHrsAndWage.dailyHours == 0)
+                            .map(dailyHrsAndWage => dailyHrsAndWage.dayNum);
+
+console.log("\n UC 11D - Non working day nums: " + nonWorkingDayStrArr);
+
+
+
+
